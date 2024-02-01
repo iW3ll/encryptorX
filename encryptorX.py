a906ca3faa34ea76e9f64e93647bb5a29f2ba7f0
Wesley Santos <84673115+iW3ll@users.noreply.github.com>
2024-01-31 23:54:55 -0300

    Add files via upload

---
+++ encryptorX.py
@@ -0,0 +1,112 @@
+import os, os.path
+import getpass
+import time
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+
+    def pad(self, s):
+        pad_len = AES.block_size - len(s) % AES.block_size
+        padding = bytes([pad_len] * pad_len)
+        return s + padding
+
+    def encrypt(self, message, key):
+        message = self.pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
+
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            plaintext = fo.read()
+        enc = self.encrypt(plaintext, self.key)
+        with open(file_name + ".enc", 'wb') as fo:
+            fo.write(enc)
+        os.remove(file_name)
+
+    def decrypt(self, ciphertext, key):
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+        pad_len = plaintext[-1]
+        return plaintext[:-pad_len]
+
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            ciphertext = fo.read()
+        dec = self.decrypt(ciphertext, self.key)
+        with open(file_name[:-4], 'wb') as fo:
+            fo.write(dec)
+        os.remove(file_name)
+
+    def getAllFiles(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dirs = []
+        for dirName, subdirList, fileList in os.walk(dir_path):
+            for fname in fileList:
+                if (fname != 'script.py' and fname != 'data.txt.enc'):
+                    dirs.append(dirName + "\\" + fname)
+        return dirs
+
+    def encrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.encrypt_file(file_name)
+
+    def decrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.decrypt_file(file_name)
+
+key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
+enc = Encryptor(key)
+clear = lambda: os.system('clear')
+
+if os.path.isfile('data.txt.enc'):
+    while True:
+        password = getpass.getpass("[!] Enter your password: ")
+        enc.decrypt_file("data.txt.enc")
+        p = ''
+        with open("data.txt", "r") as f:
+            p = f.readlines()
+        if p[0] == password:
+            enc.encrypt_file("data.txt")
+            break
+
+    while True:
+        clear()
+        choice = int(input(
+            "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the "
+            "directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
+        clear()
+        if choice == 1:
+            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+        elif choice == 2:
+            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+        elif choice == 3:
+            enc.encrypt_all_files()
+        elif choice == 4:
+            enc.decrypt_all_files()
+        elif choice == 5:
+            exit()
+        else:
+            print("Please select a valid option!")
+
+else:
+    while True:
+        clear()
+        password = getpass.getpass("Setting up stuff. Enter a password that will be used for decryption: ")
+        repassword = getpass.getpass("Confirm password: ")
+        if password == repassword:
+            break
+        else:
+            print("Passwords Mismatched!")
+    f = open("data.txt", "w+")
+    f.write(password)
+    f.close()
+    enc.encrypt_file("data.txt")
+    print("The program will restart shortly. Please re-run.")
+time.sleep(5)
